b9d2fa153d03754ca58b3f52a726f93f456e6b37
wh_Coder <461891475@qq.com>
2017-05-17 15:41:20 +0800

    koa v 0.0.1

---
+++ app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2017-05-17.
+ */
+var koa = require('koa')
+var session = require('koa-session')
+var logger = require('koa-logger')
+var bodyParser = require('koa-bodyparser')
+
+var app = koa()
+
+app.use(logger())
+app.use(session(app))
+app.use(bodyParser())
+
+app.use(function *(next) {
+  this.body = {
+    success: true
+  }
+  yield next
+})
+
+app.listen(3001)
+console.log('localhost:3001')
