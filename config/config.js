d408dedf8764423f267fe453b4d88a8c505e0aec
wh_Coder <461891475@qq.com>
2017-06-03 17:14:52 +0800

    https

---
+++ config/config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017-06-03.
+ */
+'use strict'
+
+module.exports = {
+  CLOUDINARY: {
+    'cloud_name': 'busyrat',
+    'api_key': '385948557283994',
+    'api_secret': 'Wqo_JxZki9OQTnQklQjgJ0IPIbw',
+  }
+}
